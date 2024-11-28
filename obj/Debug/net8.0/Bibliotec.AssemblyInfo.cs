42723189d65adffb091cbe8e634eceda5bf25c65
Samanta Melissa <samanta.melissa13@gmail.com>
2024-11-28 13:58:08 -0300

    Subindo tela de perfil

--- obj/Debug/net8.0/Bibliotec.AssemblyInfo.cs
+++ obj/Debug/net8.0/Bibliotec.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bibliotec")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f317b4fb07967afc9784d016f0140648cf713dea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32506cbe8e9b40a03304087658c50131de875253")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bibliotec")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bibliotec")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
